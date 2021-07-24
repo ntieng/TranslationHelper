420d6c806e126b06f00702690cffe358f2501d40
ntieng <noveltieng@gmail.com>
2021-07-24 17:31:39 +0800

    Add project files.

---
+++ TranslationHelper/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace TranslationHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Search = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnAddToFile = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Key = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.English = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Chinese = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Malay = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(675, 36);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(75, 23);
+            this.Search.TabIndex = 0;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Select Translation Folder";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(569, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // btnAddToFile
+            // 
+            this.btnAddToFile.Location = new System.Drawing.Point(789, 309);
+            this.btnAddToFile.Name = "btnAddToFile";
+            this.btnAddToFile.Size = new System.Drawing.Size(138, 23);
+            this.btnAddToFile.TabIndex = 7;
+            this.btnAddToFile.Text = "Add to File";
+            this.btnAddToFile.UseVisualStyleBackColor = true;
+            this.btnAddToFile.Click += new System.EventHandler(this.btnAddToFile_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Key,
+            this.English,
+            this.Chinese,
+            this.Malay});
+            this.dataGridView1.Location = new System.Drawing.Point(83, 138);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(844, 150);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            // 
+            // Key
+            // 
+            this.Key.HeaderText = "Key";
+            this.Key.MinimumWidth = 20;
+            this.Key.Name = "Key";
+            this.Key.Width = 200;
+            // 
+            // English
+            // 
+            this.English.HeaderText = "English";
+            this.English.Name = "English";
+            this.English.Width = 200;
+            // 
+            // Chinese
+            // 
+            this.Chinese.HeaderText = "Chinese";
+            this.Chinese.Name = "Chinese";
+            this.Chinese.Width = 200;
+            // 
+            // Malay
+            // 
+            this.Malay.HeaderText = "Malay";
+            this.Malay.Name = "Malay";
+            this.Malay.Width = 200;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pasteToolStripMenuItem,
+            this.clearToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(103, 48);
+            this.contextMenuStrip1.Click += new System.EventHandler(this.contextMenuStrip1_Click);
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(102, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 663);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnAddToFile);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Search);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnAddToFile;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Key;
+        private System.Windows.Forms.DataGridViewTextBoxColumn English;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Chinese;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Malay;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+    }
+}
+
---
+++ TranslationHelper/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TranslationHelper
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog = new OpenFileDialog();
+            using (var opnDlg = new OpenFileDialog())
+            {
+                if (opnDlg.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = Path.GetDirectoryName(opnDlg.FileName);
+                }
+            }
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count > 0)
+                dataGridView1.ContextMenuStrip = contextMenuStrip1;
+        }
+
+        private void PasteClipboard()
+        {
+            try
+            {
+                dataGridView1.Rows.Add();
+                string s = Clipboard.GetText();
+                string[] lines = s.Replace("\n", "").Split('\r');
+                string[] fields;
+                int row = 0;
+                int column = 0;
+                foreach (string l in lines)
+                {
+                    fields = l.Split('\t');
+                    foreach (string f in fields)
+                    {
+                        dataGridView1[column++, row].Value = f;
+                    }
+                    dataGridView1.Rows.Add();
+                    row++;
+                    column = 0;
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("The data you pasted is in the wrong format for the cell");
+                return;
+            }
+        }
+
+        private void contextMenuStrip1_Click(object sender, EventArgs e)
+        {
+            PasteClipboard();
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+        }
+
+        private void btnAddToFile_Click(object sender, EventArgs e)
+        {
+            //This line of code creates a text file for the data export.
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("No path directory selected");
+            }
+
+            else
+            {
+                StreamWriter file = new StreamWriter(textBox1.Text + "sample.txt");
+                try
+                {
+                    string sLine = "";
+
+                    //This for loop loops through each row in the table
+                    for (int r = 0; r <= dataGridView1.Rows.Count - 1; r++)
+                    {
+                        //This for loop loops through each column, and the row number
+                        //is passed from the for loop above.
+                        for (int c = 0; c <= dataGridView1.Columns.Count - 1; c++)
+                        {
+                            sLine = sLine + dataGridView1.Rows[r].Cells[c].Value;
+                            if (c != dataGridView1.Columns.Count - 1)
+                            {
+                                //A comma is added as a text delimiter in order
+                                //to separate each field in the text file.
+                                //You can choose another character as a delimiter.
+                                if (string.IsNullOrEmpty(sLine))
+                                {
+                                    continue;
+                                }
+                                sLine = sLine + ",";
+                            }
+                        }
+                        //The exported text is written to the text file, one line at a time.
+                        if (!string.IsNullOrEmpty(sLine))
+                        {
+                            file.WriteLine(sLine);
+                        }
+                        sLine = "";
+                    }
+
+                    file.Close();
+                    MessageBox.Show("Export Complete.", "Program Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (System.Exception err)
+                {
+                    MessageBox.Show(err.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    file.Close();
+                }
+            }
+
+        }
+    }
+}
